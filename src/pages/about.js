3ee0b57a347d7a92d8592b45124acc4854f5047d
kanovii <kkh0510ks@daum.net>
2021-09-13 20:53:17 +0900

    line

--- src/pages/about.js
+++ src/pages/about.js
@@ -4,7 +4,7 @@ import Calendar from 'react-calendar';
 import Layout from "../components/layout";
 import '../css/calendar.css';
 import {heading} from "../components/layout.module.css"
-import { portfolio_box, portfolio_img, portfolio_text_box} from "../css/portfolio.module.css"
+import {portfolio_text, portfolio_box, portfolio_img, portfolio_text_box} from "../css/portfolio.module.css"
 
 const AboutPage = ({pageHeading}) => {
   const [value,onChange] = React.useState(new Date())
@@ -16,7 +16,7 @@ const AboutPage = ({pageHeading}) => {
       <p>🍆 롤은 실버에서 벗어나지 못하고 있습니다.</p>
       <p>🍆 고기 중 최고는 양고기라고 생각합니다.</p>
       <p>🍆 양고기는 쯔란에 찍어먹는 것을 좋아합니다.</p>
-      <div className={heading}>kanovii's portfolio</div>
+      <div className={heading, portfolio_text}>kanovii's portfolio</div>
       <div className={portfolio_box}>
         <StaticImage className={portfolio_img} src="../images/movie_apollo.png" loading="eager" alt="portfolio"></StaticImage>
         <div className={portfolio_text_box}>
